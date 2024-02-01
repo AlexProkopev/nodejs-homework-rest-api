3e565ef65e9d6457a640e6d3637d9ba003e356a2
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2024-02-01 15:53:17 +0100

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { validateBody, authenticate } = require("../../services/middleware.js");
 const { schemas } = require("../../services/validationUser.js");
-const { regist, login, logOut } = require("../../services/auth.js");
+const { regist, login, logOut, currentUser } = require("../../services/auth.js");
 
 router.post("/register", validateBody(schemas.registSchema), regist);
 router.post("/login", validateBody(schemas.loginSchema), login);
-router.get("/current",authenticate,login);
+router.get("/current",authenticate,currentUser);
 router.post("/logout",authenticate,logOut);
 
 module.exports = router;
